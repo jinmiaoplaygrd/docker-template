00348a6bbd63a1cd3fb08af2c8fc34a01b209bde
Jin Miao <jinmiao@microsoft.com>
2016-10-08 11:15:51 +0800

    check for docker 1.12.1 first

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-curl -sSL https://mirror.azure.cn/repo/install-docker-engine.sh | sh -
+VERSION="1.12.1,"
+InstalledVersion=`docker -v | awk '{ print $3 }'`
+
+if [ "$InstalledVersion" != "$VERSION" ]
+then
+   curl -sSL https://mirror.azure.cn/repo/install-docker-engine.sh | sh -
+else
+   echo "Docker 1.12.1 already installed"
+fi
+
+curl -L http://mirror.azure.cn/docker-toolbox/linux/compose/1.8.1/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+service docker start

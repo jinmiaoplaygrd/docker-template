c62902cd2c77569cfbc6371dd5ecfb7708009131
Jin Miao <jinmiao@microsoft.com>
2016-10-11 13:52:42 +0800

    change docker-compose path to usr/bin

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -10,6 +10,6 @@ else
    echo "Docker 1.12.1 already installed"
 fi
 
-curl -L http://mirror.azure.cn/docker-toolbox/linux/compose/1.8.1/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+curl -L http://mirror.azure.cn/docker-toolbox/linux/compose/1.8.1/docker-compose-Linux-x86_64 > /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
 service docker start
